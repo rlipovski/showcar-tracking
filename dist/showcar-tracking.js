029879a307ccc328a7896f406677758c0be615ea
sspringer <sebastian.springer@mayflower.de>
2015-12-14 12:51:07 +0100

    handle pagename and set tracking

--- dist/showcar-tracking.js
+++ dist/showcar-tracking.js
@@ -824,14 +824,42 @@
 	
 	var as24tracking = Object.assign(Object.create(HTMLElement.prototype), {
 	
+	    dev: true,
 	    el: null,
 	    supportedActions: ['set', 'click', 'pageview'],
-	    supportedTypes: ['gtm'],
+	    supportedTypes: ['gtm', 'pagename'],
+	    reservedWords: ['as24-tracking-id', 'type', 'action', 'as24-tracking-value'],
 	
 	    createdCallback: function createdCallback() {
 	        this.el = $(this);
+	        var values = this.getAdditionalProperties();
+	
+	        var type = this.el.attr('type');
+	        var action = this.el.attr('action');
+	
+	        var args = [type, action, values];
+	        if (type === 'pagename') {
+	            args.slice(1, 1);
+	        }
+	
+	        if (!this.dev) {
+	            var _ut;
+	
+	            (_ut = ut).push.apply(_ut, args);
+	        }
 	    },
-	    getAdditionalProperties: function getAdditionalProperties() {}
+	    getAdditionalProperties: function getAdditionalProperties() {
+	        var _this = this;
+	
+	        var values = JSON.parse(this.el.attr('as24-tracking-value')) || {};
+	        values = Array.from(this.el[0].attributes).filter(function (element) {
+	            return !(_this.reservedWords.indexOf(element.nodeName) > -1);
+	        }).reduce(function (prev, curr) {
+	            prev[curr.nodeName] = curr.nodeValue;
+	            return prev;
+	        }, values);
+	        return values;
+	    }
 	});
 	
 	document.registerElement('as24-tracking', {
--- src/trackingElement.js
+++ src/trackingElement.js
@@ -1,15 +1,37 @@
 var as24tracking = Object.assign(Object.create(HTMLElement.prototype), {
 
+    dev: true,
     el: null,
     supportedActions: ['set', 'click', 'pageview'],
-    supportedTypes: ['gtm'],
+    supportedTypes: ['gtm', 'pagename'],
+    reservedWords: ['as24-tracking-id', 'type', 'action', 'as24-tracking-value'],
 
     createdCallback () {
         this.el = $(this);
+        var values = this.getAdditionalProperties();
+
+        var type = this.el.attr('type');
+        var action = this.el.attr('action');
+
+        var args = [type, action, values];
+        if (type === 'pagename') {
+            args.slice(1, 1);
+        }
+
+        if (!this.dev) {
+            ut.push(...args);
+        }
     },
 
     getAdditionalProperties() {
-
+        var values = JSON.parse(this.el.attr('as24-tracking-value')) || {};
+        values = Array.from(this.el[0].attributes)
+            .filter((element) => !(this.reservedWords.indexOf(element.nodeName) > -1))
+            .reduce((prev, curr) => {
+                prev[curr.nodeName] = curr.nodeValue;
+                return prev;
+            }, values);
+        return values;
     }
 
 });
