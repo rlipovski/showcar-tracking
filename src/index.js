0769e8b4ae6c6f0f607fadb9fc4ec247f5d2f2b9
Adam <aberesdeak@autoscout24.com>
2016-05-24 18:49:20 +0200

    fixed JS error

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ function processCommand(data) {
 
 var ut = window.ut || (window.ut = []);
 
-if (ut.push === [].prototype.push) {
+if (ut.push === Array.prototype.push) {
     ut.push = function() {
         Array.prototype.push.apply(window.ut, arguments);
         processCommand.apply({}, arguments);
