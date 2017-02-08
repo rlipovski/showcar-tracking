070a45a50e944c1780aca65a9f1f95b66a854e8c
Adam <aberesdeak@autoscout24.com>
2017-02-08 14:20:29 +0100

    preparing for travis release

---
+++ travis-release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -ev
+
+TARGET_BRANCH=release
+
+chmod 600 release_key
+eval `ssh-agent -s`
+ssh-add release_key
+
+mkdir temp-git
+cd temp-git
+
+git clone -b $TARGET_BRANCH --single-branch "git@github.com:AutoScout24/showcar-tracking.git" .
+git config user.name "Travis CI"
+git config user.email "${GIT_EMAIL}"
+git config push.default simple
+git checkout $TARGET_BRANCH
+
+cp -r ../dist .
+cp ../package.json .
+cp -r ../src .
+cp ../History.md .
+
+git add . -A
+git commit -am "Release"
+git push origin $TARGET_BRANCH
