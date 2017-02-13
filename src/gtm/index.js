432f70feaf77d33a5914845990bf5349989247dd
Adam Beres-Deak <me@bdadam.com>
2017-02-13 14:12:35 +0100

    More logging for "Incorrect pagename" exception

--- src/gtm/index.js
+++ src/gtm/index.js
@@ -14,7 +14,7 @@ function generateCommonParams(data) {
     var mergedPagename = merge({}, pagename, data);
 
     if (!mergedPagename || !mergedPagename.country || !mergedPagename.market || !mergedPagename.category || !mergedPagename.pageid) {
-        throw new Error('Incorrect pagename');
+        throw new Error('Incorrect pagename, ' + JSON.stringify(mergedPagename));
     }
 
     var commonPageName = [mergedPagename.country, mergedPagename.market, mergedPagename.category, mergedPagename.group, mergedPagename.pageid]
