59041415b9a7ffb1917fcfb6beee971d82fa5aa4
Diana Zubova <diana.zubova@scout24.com>
2018-10-11 17:18:24 +0200

    check the node version of agent

--- build.sh
+++ build.sh
@@ -27,6 +27,7 @@ upload_to_s3() {
     # aws --region "eu-west-1" s3 cp dist "s3://as24-assets-eu-west-1/${SERVICE}/" --recursive --exclude "*" --include "*-fragment.html" --cache-control "max-age=300" --acl public-read
 }
 
+node --version
 npm i
 npm run build
 
