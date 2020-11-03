f3250021fdd86097e99663f37d2d32e7da478603
Adam Beres-Deak <adam.beres-deak@autoscout24.com>
2020-11-03 17:40:43 +0100

    Simplified code for new click tracking attribute

--- src/clickTrackingAttributes.js
+++ src/clickTrackingAttributes.js
@@ -3,24 +3,25 @@
 
 window.addEventListener('click', (e) => {
     window.dataLayer = window.dataLayer || [];
+    const node = e.target.closest('[data-click-datalayer-push]');
+    
+    if (!node) {
+        return;
+    }
+    
+    const rawValue = node.getAttribute('data-click-datalayer-push');
 
-    let node = e.target;
+    if (rawValue) {
+        try {
+            // TODO: Check if we can use something like `eval` but more secure to allow JS style objects
+            // aka. objects without quotes around key names or some computation inside
+            // e.g. { event: "event_trigger", event_category: "category", event_action: "action" }
+            // Don't use `eval` or `new Function(...)` directly because they execute anything => XSS attack
 
-    do {
-        const rawValue = node.getAttribute('data-click-datalayer-push');
-        if (rawValue) {
-            try {
-                // TODO: Check if we can use something like `eval` but more secure to allow JS style objects
-                // aka. objects without quotes around key names or some computation inside
-                // e.g. { event: "event_trigger", event_category: "category", event_action: "action" }
-                // Don't use `eval` or `new Function(...)` directly because they execute anything => XSS attack
-
-                const value = JSON.parse(rawValue);
-                window.dataLayer.push(value);
-            } catch (e) {
-                console.error('Cannot parse tracking value', rawValue, node);
-            }
+            const value = JSON.parse(rawValue);
+            window.dataLayer.push(value);
+        } catch (e) {
+            console.error('Cannot parse tracking value', rawValue, node);
         }
-        node = node.parentNode;
-    } while (node && node.getAttribute);
+    }
 });
